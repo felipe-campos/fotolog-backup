b5088d9f237bdea4f799eb7e0ac845697cb67d1f
Felipe Campos <fcampos@poli.ufrj.br>
2016-03-02 01:13:51 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,81 @@
+//const http = require('http');
+//
+//var server = http.createServer( (request, response) => {
+//        response.writeHead(200, {'Content-Type': 'text/plain'});
+//        response.end('Hello World!\n');
+//      } ).listen(8124);
+//
+//console.log('Server running at http://127.0.0.1:8124/');
+
+
+// Objective: save every post of a given Fotolog, namely:
+// 
+//     - the picture posted
+//     - the description posted (which comes with post date)
+//     - every comment on the post (author + content)
+//
+//
+// Strategy to achieve Objetive:
+// 
+//     1. know every post link
+//     2. parse every post page by iterating over the links
+//     3. for each post page parsed, download Objective data and store it as follow
+// 
+//
+// File System for downloaded data:
+// 
+//  <Fotolog Name> ┐  
+//                 ├ <First Year> ───┐
+//                 │                 ├ <Month (1-12)> ───┐
+//                 ├ <Second Year>   │                   ├─ <Day (1-31)> ──┐
+//                 │                 ├ <Month (1-12)>    │                 ├─ fotolog-picture_<YYYY>-<MM>-<DD>.jpg
+//                 │       •                             ├─ <Day (1-31)>   ├─ fotolog-description_<YYYY>-<MM>-<DD>.txt
+//                 │       •                  •                            └─ fotolog-comments_<YYYY>-<MM>-<DD>.txt
+//                 │       •                  •                  •
+//                 │                          •                  •
+//                 └ <Last Year>                                 •
+//
+//
+// - A fotolog-comment .txt archive will not be created if the picture had no comments
+// - A <Year> directory will not be created if no pictures were posted in such year
+// - <Month> and <Day> directories follow the same rule. In other words: there must not be an empty directory in the FS
+
+var http = require('http');
+
+var request = require('request'),
+    cheerio = require('cheerio'),
+    fotologName = 'moderaterock',
+    fotologUrl  = 'http://www.fotolog.com/',
+    fotologMosaic = fotologUrl + fotologName + '/mosaic/',
+    fotologArq  = fotologUrl + fotologName + '/arquive/'; // TRASH PROBABLY
+
+request(fotologMosaic, (err, resp, body) => {
+//  console.log('ERR:\n' + err);
+//  console.log('RESP:\n' + resp);
+//  console.log('BODY:\n' + body);
+//  var $ = cheerio.load('#wall_column_left', body),
+//  var $ = cheerio.load('#wall_column_left', 'body', body),
+  var $ = cheerio.load(body),
+      $pagination = $('#pagination'),
+      currentPage = $pagination.children('strong'),
+      pagination = $pagination.get(0);
+//      currentPage = pagination
+//  $pagination = $('#pagination');
+  console.log(currentPage.text());
+//  console.log('\n####################\n$PAGINATION####################');
+//  console.log($pagination);
+//  console.log('Estamos na página ' + $pagination.find('strong').html() + '.\n');
+  
+});
+
+var server = http.createServer( (request, response) => {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('Hello World!\n');
+      } ).listen(8124);
+
+//var server = http.createServer( (request, response) => {
+//        response.writeHead(200, {'Content-Type': 'text/plain'});
+//        response.end('Hello World!\n');
+//      } ).listen(8124);
+
+console.log('Server running at http://127.0.0.1:8124/');
---
+++ objetos.js
@@ -0,0 +1,173 @@
+
+// currentPage = 
+{
+  '0': {
+    type: 'tag',
+    name: 'strong',
+    attribs: {},
+    children: [[Object]],
+    next: {
+      data: ' ',
+      type: 'text',
+      next: [Object],
+      prev: [Circular],
+      parent: [Object]
+    },
+    prev: {
+      data: ' ',
+      type: 'text',
+      next: [Circular],
+      prev: null,
+      parent: [Object]
+    },
+    parent: {
+      type: 'tag',
+      name: 'div',
+      attribs: [Object],
+      children: [Object],
+      next: null,
+      prev: [Object],
+      parent: [Object]
+    }
+  },
+  options: {
+    withDomLvl1: true,
+    normalizeWhitespace: false,
+    xmlMode: false,
+    decodeEntities: true
+  },
+  _root: {
+    '0': {
+      type: 'root',
+      name: 'root',
+      attribs: {},
+      children: [Object],
+      next: null,
+      prev: null,
+      parent: null
+    },
+    options: {
+      withDomLvl1: true,
+      normalizeWhitespace: false,
+      xmlMode: false,
+      decodeEntities: true
+    },
+    length: 1,
+    _root: [Circular]
+  },
+  length: 1,
+  prevObject: {
+    '0': {
+      type: 'tag',
+      name: 'div',
+      attribs: [Object],
+      children: [Object],
+      next: null,
+      prev: [Object],
+      parent: [Object]
+    },
+    options: {
+      withDomLvl1: true,
+      normalizeWhitespace: false,
+      xmlMode: false,
+      decodeEntities: true
+    },
+    _root: {
+      '0': [Object],
+      options: [Object],
+      length: 1,
+      _root: [Circular]
+    },
+    length: 1,
+    prevObject: {
+      '0': [Object],
+      options: [Object],
+      length: 1,
+      _root: [Circular]
+    }
+  }
+}
+
+
+
+// $pagination = 
+{
+  '0': {
+    type: 'tag',
+    name: 'div',
+    attribs: {
+      id: 'pagination'
+    },
+    children: [[Object],
+        [Object],
+        [Object],
+        [Object],
+        [Object],
+        [Object],
+        [Object]],
+    next: null,
+    prev: {
+      type: 'tag',
+      name: 'div',
+      attribs: [Object],
+      children: [Object],
+      next: [Circular],
+      prev: [Object],
+      parent: [Object]
+    },
+    parent: {
+      type: 'tag',
+      name: 'div',
+      attribs: [Object],
+      children: [Object],
+      next: [Object],
+      prev: [Object],
+      parent: [Object]
+    }
+  },
+  options: {
+    withDomLvl1: true,
+    normalizeWhitespace: false,
+    xmlMode: false,
+    decodeEntities: true
+  },
+  _root: {
+    '0': {
+      type: 'root',
+      name: 'root',
+      attribs: {},
+      children: [Object],
+      next: null,
+      prev: null,
+      parent: null
+    },
+    options: {
+      withDomLvl1: true,
+      normalizeWhitespace: false,
+      xmlMode: false,
+      decodeEntities: true
+    },
+    length: 1,
+    _root: [Circular]
+  },
+  length: 1,
+  prevObject: {
+    '0': {
+      type: 'root',
+      name: 'root',
+      attribs: {},
+      children: [Object],
+      next: null,
+      prev: null,
+      parent: null
+    },
+    options: {
+      withDomLvl1: true,
+      normalizeWhitespace: false,
+      xmlMode: false,
+      decodeEntities: true
+    },
+    length: 1,
+    _root: [Circular]
+  }
+}
